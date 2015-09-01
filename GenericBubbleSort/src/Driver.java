733d8fb8663418d03cf494cec7b32c3031a6f1e4
Frode Naesje <fn@hin.no>
2015-09-01 21:01:49 +0200

    Changed Driver

--- GenericBubbleSort/src/Driver.java
+++ GenericBubbleSort/src/Driver.java
@@ -1,9 +1,8 @@
 
 public class Driver {
 	public static void main(String[] args) {
-	    GenericBubbleSort gbs = new GenericBubbleSort();
-		Integer[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
-	    gbs.bubbleSort(list);
+	    Integer[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+	    GenericBubbleSort.bubbleSort(list);
 	    for (int i = 0; i < list.length; i++)
 	      System.out.print(list[i] + " ");
 	}
