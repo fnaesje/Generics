b9a8e1cb3dc074130347a104967658f7b32cf165
Frode Naesje <fn@hin.no>
2015-09-02 14:55:40 +0200

    Added parameterized test

--- MaxElementInArray/test/ParameterizedMaxElementInArrayTest.java
+++ MaxElementInArray/test/ParameterizedMaxElementInArrayTest.java
@@ -1,20 +1,41 @@
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-import org.junit.Before;
 import org.junit.Test;
+
 @RunWith(Parameterized.class)
 public class ParameterizedMaxElementInArrayTest {
+	private Integer[] input;
+	private int expected;
+	
+	public ParameterizedMaxElementInArrayTest(Integer[] input, Integer expected) {
+		super();
+		this.input = input;
+		this.expected = expected;
+	}
 
-	@Before
-	public void setUp() throws Exception {
+	@Parameters
+	public static Collection<Object[]> data() {
+	      return Arrays.asList(new Object[][] {
+	         { new Integer[] { 1, 2, 3 }, 3},
+	         { new Integer[] { -1, -2, -3 }, -1},
+	         { new Integer[] { 1 }, 1},
+	         { new Integer[] { 1, 1, 1 }, 1},
+	         { new Integer[] { 1,-1}, 1 }	         
+	         });
+ 
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	 public void testVariousInput() {
+	  	  assertThat(MaxElements.max(input),equalTo(expected));
 	}
 
 }
